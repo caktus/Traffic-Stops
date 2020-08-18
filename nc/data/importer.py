985a520a7bbe342589b84c27ac8b7fd0e1e340b5
Jeremy Giboson <jgibson@caktusgroup.com>
2020-08-18 13:36:57 -0400

    [FJ-8] Moves SQL statements to separate file. flake8 fixes

--- nc/data/importer.py
+++ nc/data/importer.py
@@ -16,6 +16,7 @@ import pytz
 from tsdata.dataset_facts import compute_dataset_facts
 from tsdata.sql import drop_constraints_and_indexes
 from tsdata.utils import call, flush_memcached, line_count, download_and_unzip_data, unzip_data
+from nc.data import copy_nc
 from nc.models import Agency, Search, Person, Stop, Contraband, SearchBasis
 from nc.prime_cache import run as prime_cache_run
 from .download_from_nc import nc_download_and_unzip_data
@@ -270,80 +271,28 @@ def update_nc_agencies(nc_csv_path, destination):
     email.send()
     return new_nc_csv_path
 
+
 def copy_from(destination, nc_csv_path):
-    """Execute copy.sql to COPY csv data files into PostgreSQL database"""
-    SET_TIMEZONE = f"SET TIMEZONE='{settings.NC_TIME_ZONE}'"
-    CLEAN_DATABASE = """
-        TRUNCATE "nc_stop" RESTART IDENTITY CASCADE;
-        TRUNCATE "nc_person" RESTART IDENTITY CASCADE;
-        TRUNCATE "nc_search" RESTART IDENTITY CASCADE;
-        TRUNCATE "nc_searchbasis" RESTART IDENTITY CASCADE;
-        TRUNCATE "nc_contraband" RESTART IDENTITY CASCADE;
-        TRUNCATE "nc_agency" RESTART IDENTITY CASCADE;
-    """
-    NC_SQL_COPY = {
-        "Stop.csv": "COPY nc_stop (stop_id, agency_description, date, purpose, action, driver_arrest, passenger_arrest, encounter_force, engage_force, officer_injury, driver_injury, passenger_injury, officer_id, stop_location, stop_city) FROM STDIN WITH  DELIMITER ',' NULL AS '' CSV HEADER FORCE NOT NULL officer_id, stop_city, stop_location",
-        "PERSON.csv": "COPY nc_person (person_id, stop_id, type, age, gender, ethnicity, race) FROM STDIN WITH DELIMITER ',' NULL AS '' CSV HEADER FORCE NOT NULL ethnicity, gender, race",
-        "Search.csv": "COPY nc_search (search_id, stop_id, person_id, type, vehicle_search, driver_search, passenger_search, property_search, vehicle_siezed, personal_property_siezed, other_property_sized) FROM STDIN WITH DELIMITER ',' NULL AS '' CSV HEADER",
-        "Contraband.csv": "COPY nc_contraband (contraband_id, search_id, person_id, stop_id, ounces, pounds, pints, gallons, dosages, grams, kilos, money, weapons, dollar_amount) FROM STDIN WITH DELIMITER ',' CSV HEADER",
-        "SearchBasis.csv": "COPY nc_searchbasis (search_basis_id, search_id, person_id, stop_id, basis) FROM STDIN WITH DELIMITER ',' CSV HEADER",
-        "NC_agencies.csv": "COPY  nc_agency (id, name, census_profile_id) FROM STDIN WITH DELIMITER ',' CSV HEADER FORCE NOT NULL census_profile_id"
-    }
+    """Populates the NC database from csv files."""
 
-    FINALIZE_COPY = """
-        UPDATE nc_stop SET agency_id = nc_agency.id FROM nc_agency WHERE nc_stop.agency_description = nc_agency.name;
+    conn = psycopg2.connect(
+        database="traffic_stops_nc",
+        user=os.environ.get("PGUSER"),
+        host=os.environ.get("PGHOST"),
+        port=os.environ.get("PGPORT")
+    )
 
-        ANALYZE;
-
-        ALTER TABLE "public"."nc_stop" ADD CONSTRAINT "nc_stop_pkey" PRIMARY KEY (stop_id);
-        ALTER TABLE "public"."nc_searchbasis" ADD CONSTRAINT "nc_searchbasis_pkey" PRIMARY KEY (search_basis_id);
-        ALTER TABLE "public"."nc_search" ADD CONSTRAINT "nc_search_pkey" PRIMARY KEY (search_id);
-        ALTER TABLE "public"."nc_person" ADD CONSTRAINT "nc_person_pkey" PRIMARY KEY (person_id);
-        ALTER TABLE "public"."nc_contraband" ADD CONSTRAINT "nc_contraband_pkey" PRIMARY KEY (contraband_id);
-        ALTER TABLE "public"."nc_agency" ADD CONSTRAINT "nc_agency_pkey" PRIMARY KEY (id);
-        ALTER TABLE "public"."nc_stop" ADD CONSTRAINT "nc_stop_stop_id_check" CHECK ((stop_id >= 0));
-        ALTER TABLE "public"."nc_stop" ADD CONSTRAINT "nc_stop_purpose_check" CHECK ((purpose >= 0));
-        ALTER TABLE "public"."nc_stop" ADD CONSTRAINT "nc_stop_action_check" CHECK ((action >= 0));
-        ALTER TABLE "public"."nc_search" ADD CONSTRAINT "nc_search_type_check" CHECK ((type >= 0));
-        ALTER TABLE "public"."nc_person" ADD CONSTRAINT "nc_person_age_check" CHECK ((age >= 0));
-        ALTER TABLE "public"."nc_stop" ADD CONSTRAINT "nc_stop_agency_id_f050f703620f44c_fk_nc_agency_id" FOREIGN KEY (agency_id) REFERENCES nc_agency(id) DEFERRABLE INITIALLY DEFERRED;
-        ALTER TABLE "public"."nc_searchbasis" ADD CONSTRAINT "nc_searchbasis_stop_id_36f52da3736812d4_fk_nc_stop_stop_id" FOREIGN KEY (stop_id) REFERENCES nc_stop(stop_id) DEFERRABLE INITIALLY DEFERRED;
-        ALTER TABLE "public"."nc_searchbasis" ADD CONSTRAINT "nc_searchbasi_search_id_3e2dcf5dc9fc212f_fk_nc_search_search_id" FOREIGN KEY (search_id) REFERENCES nc_search(search_id) DEFERRABLE INITIALLY DEFERRED;
-        ALTER TABLE "public"."nc_searchbasis" ADD CONSTRAINT "nc_searchbasi_person_id_3500809179032efb_fk_nc_person_person_id" FOREIGN KEY (person_id) REFERENCES nc_person(person_id) DEFERRABLE INITIALLY DEFERRED;
-        ALTER TABLE "public"."nc_search" ADD CONSTRAINT "nc_search_stop_id_631cf86a83f3528_fk_nc_stop_stop_id" FOREIGN KEY (stop_id) REFERENCES nc_stop(stop_id) DEFERRABLE INITIALLY DEFERRED;
-        ALTER TABLE "public"."nc_search" ADD CONSTRAINT "nc_search_person_id_13b611eaa9879eb9_fk_nc_person_person_id" FOREIGN KEY (person_id) REFERENCES nc_person(person_id) DEFERRABLE INITIALLY DEFERRED;
-        ALTER TABLE "public"."nc_person" ADD CONSTRAINT "nc_person_stop_id_391c330ed82da305_fk_nc_stop_stop_id" FOREIGN KEY (stop_id) REFERENCES nc_stop(stop_id) DEFERRABLE INITIALLY DEFERRED;
-        ALTER TABLE "public"."nc_contraband" ADD CONSTRAINT "nc_contraband_stop_id_77ce6cabcbe40c3c_fk_nc_stop_stop_id" FOREIGN KEY (stop_id) REFERENCES nc_stop(stop_id) DEFERRABLE INITIALLY DEFERRED;
-        ALTER TABLE "public"."nc_contraband" ADD CONSTRAINT "nc_contraband_search_id_7ead089372beb55f_fk_nc_search_search_id" FOREIGN KEY (search_id) REFERENCES nc_search(search_id) DEFERRABLE INITIALLY DEFERRED;
-        ALTER TABLE "public"."nc_contraband" ADD CONSTRAINT "nc_contraband_person_id_50e4f1b98b0285ab_fk_nc_person_person_id" FOREIGN KEY (person_id) REFERENCES nc_person(person_id) DEFERRABLE INITIALLY DEFERRED;
-        CREATE INDEX nc_contraband_5fad4402 ON nc_contraband USING btree (search_id);
-        CREATE INDEX nc_contraband_91455da7 ON nc_contraband USING btree (stop_id);
-        CREATE INDEX nc_contraband_a8452ca7 ON nc_contraband USING btree (person_id);
-        CREATE INDEX nc_person_91455da7 ON nc_person USING btree (stop_id);
-        CREATE INDEX nc_search_91455da7 ON nc_search USING btree (stop_id);
-        CREATE INDEX nc_search_a8452ca7 ON nc_search USING btree (person_id);
-        CREATE INDEX nc_searchbasis_5fad4402 ON nc_searchbasis USING btree (search_id);
-        CREATE INDEX nc_searchbasis_91455da7 ON nc_searchbasis USING btree (stop_id);
-        CREATE INDEX nc_searchbasis_a8452ca7 ON nc_searchbasis USING btree (person_id);
-        CREATE INDEX nc_stop_169fc544 ON nc_stop USING btree (agency_id);
-        CREATE INDEX nc_stop_date_7d643c8a9c590bf7_uniq ON nc_stop USING btree (date);
-
-        ANALYZE;
-        COMMIT;
-    """
-    conn = psycopg2.connect(database="traffic_stops_nc", user=os.environ.get("PGUSER"), host=os.environ.get("PGHOST"), port=os.environ.get("PGPORT"))
     with conn:
         with conn.cursor() as cur:
-            cur.execute(SET_TIMEZONE)
-            cur.execute(CLEAN_DATABASE)
+            cur.execute(copy_nc.SET_TIMEZONE)
+            cur.execute(copy_nc.CLEAN_DATABASE)
             path = Path(destination)
             for p in path.glob("*.csv"):
-                if p.name in NC_SQL_COPY.keys():
+                if p.name in copy_nc.NC_COPY_INSTRUCTIONS.keys():
                     with p.open() as fh:
                         logger.info(f"INSERTING {p.name} into the database")
-                        cur.copy_expert(NC_SQL_COPY[p.name], fh)
-            cur.execute(FINALIZE_COPY)
-            
+                        cur.copy_expert(copy_nc.NC_COPY_INSTRUCTIONS[p.name], fh)
+            cur.execute(copy_nc.FINALIZE_COPY)
 
     # Remove all stops and related objects that are before 1 Jan 2002, when everyone
     # started reporting consistently.  Don't clear out the NC State Highway Patrol
