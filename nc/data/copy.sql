de731533c6d21f46b5c0874a2ba616e4b23d8803
Jeremy Giboson <jgibson@caktusgroup.com>
2020-08-18 13:16:18 -0400

    [FJ-8] Changes import process to psycopg2 rather than SQL file.

--- nc/data/copy.sql
+++ nc/data/copy.sql
@@ -1,4 +1,5 @@
 -- http://www.postgresql.org/docs/current/interactive/populate.html
+-- As of 2020 this file is no longer being used because the deployment transistioned to RDS.
 
 \set ON_ERROR_STOP true
 
@@ -15,43 +16,46 @@ TRUNCATE "nc_contraband" RESTART IDENTITY CASCADE;
 TRUNCATE "nc_agency" RESTART IDENTITY CASCADE;
 
 -- import stops
-\set import_file :data_dir '/test.sql'
-\i '/tmp/test.sql'
--- \copy nc_stop (stop_id, agency_description, date, purpose, action, driver_arrest, passenger_arrest, encounter_force, engage_force, officer_injury, driver_injury, passenger_injury, officer_id, stop_location, stop_city) FROM '/home/jgibson/local_dev/tsc/active_projects/Traffic-Stops/ncdata/Stop.csv' WITH DELIMITER ',' NULL AS '' CSV HEADER FORCE NOT NULL officer_id, stop_city, stop_location
+\set import_file :data_dir '/Stop.csv'
+COPY nc_stop (stop_id, agency_description, date, purpose, action, driver_arrest, passenger_arrest, encounter_force, engage_force, officer_injury, driver_injury, passenger_injury, officer_id, stop_location, stop_city) FROM :'import_file' WITH
+    DELIMITER ','
+    NULL AS ''
+    CSV HEADER
+    FORCE NOT NULL officer_id, stop_city, stop_location;
 
--- -- import persons
--- \set import_file :data_dir '/PERSON.csv'
--- COPY nc_person (person_id, stop_id, type, age, gender, ethnicity, race) FROM :'import_file' WITH
---     DELIMITER ','
---     NULL AS ''
---     CSV HEADER
---     FORCE NOT NULL ethnicity, gender, race; --required; might be an issue later on...
+-- import persons
+\set import_file :data_dir '/PERSON.csv'
+COPY nc_person (person_id, stop_id, type, age, gender, ethnicity, race) FROM :'import_file' WITH
+    DELIMITER ','
+    NULL AS ''
+    CSV HEADER
+    FORCE NOT NULL ethnicity, gender, race; --required; might be an issue later on...
 
--- -- import searches
--- \set import_file :data_dir '/Search.csv'
--- COPY nc_search (search_id, stop_id, person_id, type, vehicle_search, driver_search, passenger_search, property_search, vehicle_siezed, personal_property_siezed, other_property_sized) FROM :'import_file' WITH
---     DELIMITER ','
---     NULL AS ''
---     CSV HEADER;
+-- import searches
+\set import_file :data_dir '/Search.csv'
+COPY nc_search (search_id, stop_id, person_id, type, vehicle_search, driver_search, passenger_search, property_search, vehicle_siezed, personal_property_siezed, other_property_sized) FROM :'import_file' WITH
+    DELIMITER ','
+    NULL AS ''
+    CSV HEADER;
 
--- -- import contraband
--- \set import_file :data_dir '/Contraband.csv'
--- COPY nc_contraband (contraband_id, search_id, person_id, stop_id, ounces, pounds, pints, gallons, dosages, grams, kilos, money, weapons, dollar_amount) FROM :'import_file' WITH
---     DELIMITER ','
---     CSV HEADER;
+-- import contraband
+\set import_file :data_dir '/Contraband.csv'
+COPY nc_contraband (contraband_id, search_id, person_id, stop_id, ounces, pounds, pints, gallons, dosages, grams, kilos, money, weapons, dollar_amount) FROM :'import_file' WITH
+    DELIMITER ','
+    CSV HEADER;
 
--- -- import search-basis
--- \set import_file :data_dir '/SearchBasis.csv'
--- COPY nc_searchbasis (search_basis_id, search_id, person_id, stop_id, basis) FROM :'import_file' WITH
---     DELIMITER ','
---     CSV HEADER;
+-- import search-basis
+\set import_file :data_dir '/SearchBasis.csv'
+COPY nc_searchbasis (search_basis_id, search_id, person_id, stop_id, basis) FROM :'import_file' WITH
+    DELIMITER ','
+    CSV HEADER;
 
--- -- -- populate nc_agency lookup table, preserving agency id from CSV
+-- -- populate nc_agency lookup table, preserving agency id from CSV
 
--- COPY nc_agency (id, name, census_profile_id) from :'nc_csv_table' WITH
---     DELIMITER ','
---     CSV HEADER
---     FORCE NOT NULL census_profile_id;
+COPY nc_agency (id, name, census_profile_id) from :'nc_csv_table' WITH
+    DELIMITER ','
+    CSV HEADER
+    FORCE NOT NULL census_profile_id;
 
 -- populate nc_stop.agency_id foreign key
 UPDATE nc_stop SET agency_id = nc_agency.id
