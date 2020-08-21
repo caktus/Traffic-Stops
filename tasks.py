7f7100e6498ee3719e1dd3dceef718770ec7dd73
Colin Copeland <copelco@caktusgroup.com>
2020-08-21 10:48:14 -0400

    [FJ-9] add production env

--- tasks.py
+++ tasks.py
@@ -12,14 +12,10 @@ def staging(c):
     c.config.namespace = "trafficstops-staging"
 
 
-@invoke.task()
-def up(c, build=False):
-    """Test production image locally using docker-compose
-    """
-    if build:
-        kubesae.image["tag"](c)
-        kubesae.image["build"](c)
-    c.run("docker-compose up --remove-orphans")
+@invoke.task
+def production(c):
+    c.config.env = "production"
+    c.config.namespace = "trafficstops-production"
 
 
 @invoke.task
@@ -34,8 +30,8 @@ ns.add_collection(kubesae.aws)
 ns.add_collection(kubesae.deploy)
 ns.add_collection(kubesae.pod)
 ns.add_task(staging)
+ns.add_task(production)
 ns.add_task(ansible_playbook, "playbook")
-ns.add_task(up)
 ns.configure(
     {
         "app": "trafficstops_app",
