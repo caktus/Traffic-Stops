37c9367a152a9dff455e77bea14f2244a84063d0
Colin Copeland <copelco@caktusgroup.com>
2020-08-21 10:54:24 -0400

    [FJ-9] add post-stats task

--- tasks.py
+++ tasks.py
@@ -1,3 +1,4 @@
+import yaml
 import invoke
 import kubesae
 from colorama import init
@@ -19,6 +20,19 @@ def production(c):
 
 
 @invoke.task
+def pod_stats(c):
+    """Report total pods vs pod capacity."""
+    nodes = yaml.safe_load(c.run("kubectl get nodes -o yaml", hide="out").stdout)
+    pod_capacity = sum([int(item["status"]["capacity"]["pods"]) for item in nodes["items"]])
+    pod_total = c.run(
+        "kubectl get pods --all-namespaces | grep Running | wc -l", hide="out"
+    ).stdout.strip()
+    print(f"Running pods: {pod_total}")
+    print(f"Maximum pods: {pod_capacity}")
+    print(f"Total nodes: {len(nodes['items'])}")
+
+
+@invoke.task
 def ansible_playbook(c, name, extra="", verbosity=1):
     with c.cd("deploy/"):
         c.run(f"ansible-playbook {name} {extra} -{'v'*verbosity}")
@@ -31,6 +45,7 @@ ns.add_collection(kubesae.deploy)
 ns.add_collection(kubesae.pod)
 ns.add_task(staging)
 ns.add_task(production)
+ns.add_task(pod_stats)
 ns.add_task(ansible_playbook, "playbook")
 ns.configure(
     {
