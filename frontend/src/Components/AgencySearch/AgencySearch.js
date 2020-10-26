2086c72997780d34cc5fb88bac660195fe90619b
Michael Ashton <michael.ashton@caktusgroup.com>
2020-10-26 13:29:26 -0400

    [FJ-61] add fetch-reducer

--- frontend/src/Components/AgencySearch/AgencySearch.js
+++ frontend/src/Components/AgencySearch/AgencySearch.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { AgencySearchStyled, AgenciesList, AgencyListItem } from './AgencySearch.styled';
+
+// Router
 import { Link, useRouteMatch } from 'react-router-dom';
+
+// STate
+import fetchReducer, {
+  initialState,
+  FETCH_START,
+  FETCH_SUCCESS,
+  FETCH_FAILURE,
+} from 'Context/fetch-reducer';
 
 function AgencySearch(props) {
   const match = useRouteMatch();
+  const [state, dispatch] = useReducer(fetchReducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: FETCH_START });
+    async function _fetchAgencyList() {
+      try {
+      } catch (error) {}
+    }
+  }, []);
+
   return (
     <AgencySearchStyled data-testid="AgencySearch">
-      <h1>Agencies</h1>
+      <h1>Find a Law Enforcement Agency</h1>
       <AgenciesList>
         <AgencyListItem>
           <Link to={`${match.url}/80`}>Durm Police Department</Link>
