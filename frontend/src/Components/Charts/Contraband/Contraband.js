7967e7602ebd4e309785d0edaf09553ac2a34215
Michael Ashton <michael.ashton@caktusgroup.com>
2021-01-26 13:55:06 -0500

    [FJ-90] finish basic page setup for remaining charts

--- frontend/src/Components/Charts/Contraband/Contraband.js
+++ frontend/src/Components/Charts/Contraband/Contraband.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './Contraband.styled';
 
 // Children
+import { P } from 'styles/StyledComponents/Typography';
 import ChartHeader from 'Components/Charts/ChartSections/ChartHeader';
+import Legend from 'Components/Charts/ChartSections/Legend/Legend';
+import DataSubsetPicker from 'Components/Charts/ChartSections/DataSubsetPicker/DataSubsetPicker';
 
-function Contraband() {
+function SearchRate() {
+  const [year, setYear] = useState();
+  const [ethnicGroupKeys, setEthnicGroupKeys] = useState([]);
+
   const handleViewData = () => {
     alert('view data');
   };
@@ -13,15 +19,47 @@ function Contraband() {
     alert('share graph');
   };
 
+  const handleKeySelected = (eg) => {
+    console.log('ethnic group: ', eg);
+  };
+
+  const handleYearSelected = (y) => {
+    console.log('year: ', y);
+  };
+
   return (
     <S.Contraband>
-      <ChartHeader
-        chartTitle="Contraband"
-        handleViewData={handleViewData}
-        handleShareGraph={handleShareGraph}
-      />
+      <S.ChartSection>
+        <ChartHeader
+          chartTitle='Contraband "Hit Rate"'
+          handleViewData={handleViewData}
+          handleShareGraph={handleShareGraph}
+        />
+        <P>
+          Shows what percentage of searches discovered contraband for a given race / ethnic group
+        </P>
+        <S.ChartSubsection>
+          <S.LineWrapper>Line graph goes here.</S.LineWrapper>
+          <S.LegendBelow>
+            <S.Spacing>
+              <Legend
+                heading="Show on graph:"
+                keys={ethnicGroupKeys}
+                onKeySelect={handleKeySelected}
+                showNonHispanic
+              />
+              <DataSubsetPicker
+                label="Year"
+                value={year}
+                onChange={handleYearSelected}
+                options={[]}
+              />
+            </S.Spacing>
+          </S.LegendBelow>
+        </S.ChartSubsection>
+      </S.ChartSection>
     </S.Contraband>
   );
 }
 
-export default Contraband;
+export default SearchRate;
