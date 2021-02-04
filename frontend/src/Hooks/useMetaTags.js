0cac8dc1e2e22cc3bab2561ef445c2729bd95cc5
Michael Ashton <michael.ashton@caktusgroup.com>
2021-02-04 10:12:57 -0500

    [FJ-96] create useMetaTags hook for easy helmet integration for OG meta tags

--- frontend/src/Hooks/useMetaTags.js
+++ frontend/src/Hooks/useMetaTags.js
@@ -1,37 +1,49 @@
 import React from 'react';
 
-function useMetaTags(agencyDetails, isOfficer) {
-  console.log('agencyDetails', agencyDetails);
+import CopWatchLogoSolid from 'img/NC_copwatch_logo_solid.png';
 
-  const _getEntityReference = () => {};
+// Hooks
+import useOfficerId from 'Hooks/useOfficerId';
+
+// State
+import { useChartState } from 'Context/chart-state';
+import { AGENCY_DETAILS } from './useDataset';
+
+// Deps
+import { Helmet } from 'react-helmet';
+
+function useMetaTags() {
+  const officerId = useOfficerId();
+  const [state] = useChartState();
+
+  const _getEntityReference = () => {
+    return officerId ? `Officer ${officerId}` : state.data[AGENCY_DETAILS].name;
+  };
+
+  const _getPageTitle = () => {
+    return `Stop Data for ${_getEntityReference()}`;
+  };
+  const _getUrlForShare = () => {
+    return window.location.href;
+  };
 
   const _renderMetaTags = () => {
     return (
-      <>
-        <title>Toots</title>
-        <meta
-          property="og:title"
-          content="Traffic Stop Data for {% if officer_id %} Officer {{ officer_id }} at the {% endif %} {{ object }}"
-        />
+      <Helmet>
+        <title>{_getPageTitle()}</title>
+        <meta property="og:title" content={`Traffic Stop Data for ${_getPageTitle()}`} />
         <meta
           property="og:description"
-          content="NC Copwatch collects traffic stop data reported by policing agencies across the state. {% if officer_id %} Officer {{ officer_id }}{% else %}{{ object }}{% endif %} has traffic stop data available for analysis at nccopwatch.org."
+          content={`NC Copwatch collects traffic stop data reported by policing agencies across the state. ${_getEntityReference()} has traffic stop data available for analysis at nccopwatch.org.`}
         />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="{{ request.build_absolute_uri }}" />
-        <meta
-          property="og:image"
-          content="{{ request.scheme }}://{{ request.get_host}}{% static 'img/NC_copwatch_social_share_logo.png' %}"
-        />
-        <meta
-          property="og:image:secure_url"
-          content="{{ request.scheme }}://{{ request.get_host}}{% static 'img/NC_copwatch_social_share_logo.png' %}"
-        />
+        <meta property="og:url" content={_getUrlForShare()} />
+        <meta property="og:image" content={CopWatchLogoSolid} />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:alt" content="The NC CopWatch Logo" />
         <meta property="og:image:width" content="600" />
         <meta property="og:image:height" content="300" />
-      </>
+      </Helmet>
     );
   };
 
