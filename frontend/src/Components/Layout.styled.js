0627bab6a1070d0fe60c2806b7190872d05bf2d9
Aristotel Fani <afani1997@gmail.com>
2022-09-15 18:58:40 -0400

    remove fixed header

--- frontend/src/Components/Layout.styled.js
+++ frontend/src/Components/Layout.styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export default styled.div`
   width: 100%;
   display: flex;
+  flex: 1;
   flex-direction: column;
-  max-height: 100vh;
+  max-height: 100%;
 `;
