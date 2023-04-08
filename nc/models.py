026a239fa2660826dc344f6aea3f2ed3e4ef49bc
Colin Copeland <copelco@caktusgroup.com>
2023-04-08 14:04:13 +0000

    truncate to month

--- nc/models.py
+++ nc/models.py
@@ -168,8 +168,7 @@ STOP_SUMMARY_VIEW_SQL = """
     SELECT
         ROW_NUMBER() OVER () AS id
         , "nc_stop"."agency_id"
-        , DATE_PART('year', DATE_TRUNC('year', date AT TIME ZONE 'America/New_York'))::integer AS "year"
-        , "nc_stop"."date"
+        , DATE_TRUNC('month', date AT TIME ZONE 'America/New_York') AS "date"
         , "nc_stop"."purpose" AS "stop_purpose"
         , "nc_stop"."engage_force"
         , "nc_search"."type" AS "search_type"
@@ -189,8 +188,8 @@ STOP_SUMMARY_VIEW_SQL = """
     LEFT OUTER JOIN "nc_contraband"
         ON ("nc_stop"."stop_id" = "nc_contraband"."stop_id")
     GROUP BY
-        2, 3, 4, 5, 6, 7, 8, 9, 10, 11
-    ORDER BY "agency_id", "year" ASC;
+        2, 3, 4, 5, 6, 7, 8, 9, 10
+    ORDER BY "agency_id", "date" ASC;
 """  # noqa
 
 
@@ -202,7 +201,6 @@ class StopSummary(pg.ReadOnlyMaterializedView):
     with_data = False
 
     id = models.PositiveIntegerField(primary_key=True)
-    year = models.IntegerField()
     date = models.DateTimeField()
     agency = models.ForeignKey("Agency", on_delete=models.DO_NOTHING)
     stop_purpose = models.PositiveSmallIntegerField(choices=PURPOSE_CHOICES)
