aebbe320f387cc93bfac1d4c56ef00ac615fa9be
Aristotel Fani <afani@caktusgroup.com>
2023-04-11 12:34:25 -0400

    Add resource file model (#184)
    * add resource file model
    
    * serve media files locally
    
    * require ResourceFile.file; re-create migration
    
    * create files.nccopwatch.org CF distribution
    
    * install django-storages
    
    * use filter_horizontal for agencies m2m
    
    * configure deploy to use files.nccopwatch.org
    
    * add pre-commit fix
    
    * remove cpu limit
    
    * install boto3 in base reqs
    
    * Add resource file name to view in frontend
    
    ---------
    
    Co-authored-by: Colin Copeland <copelco@caktusgroup.com>

--- frontend/src/Components/ResourcePage/ResourcePage.js
+++ frontend/src/Components/ResourcePage/ResourcePage.js
@@ -26,11 +26,35 @@ function Resource({ resource }) {
           ))}
         </div>
 
-        {!!resource.view_more_link && (
-          <div style={{ marginTop: '30px' }}>
-            <LinkButton href={resource.view_more_link} title="View More" />
-          </div>
-        )}
+        <div
+          style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap' }}
+        >
+          {!!resource.view_more_link && (
+            <div style={{ marginTop: '30px', marginBottom: '15px' }}>
+              <LinkButton href={resource.view_more_link} title="View More" />
+            </div>
+          )}
+          {resource.resource_files.map((rf) => (
+            <a href={rf.url} target="_blank" rel="noreferrer" style={{ marginRight: '10px' }}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className=""
+                style={{ width: '25px', height: '25px', paddingTop: '10px' }}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
+                />
+              </svg>
+              {rf.name}
+            </a>
+          ))}
+        </div>
       </div>
     </S.ResourceBlock>
   );
