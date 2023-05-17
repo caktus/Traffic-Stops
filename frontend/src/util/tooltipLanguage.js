e8557d43475f4760e70774ff21631b7a89485e5c
Aristotel Fani <afani@caktusgroup.com>
2023-05-17 08:42:06 -0400

    Update average tooltip language (#202)

--- frontend/src/util/tooltipLanguage.js
+++ frontend/src/util/tooltipLanguage.js
@@ -1,6 +1,7 @@
 export const tooltipLanguage = (stopCause) =>
   ({
-    Average: '',
+    Average:
+      'This section shows the likelihood that drivers of a particular race / ethnicity are searched compared to white drivers for all types of stops. ',
     Checkpoint:
       'A checkpoint can be a barrier, or a manned entrance, where drivers are subjected to a security check, often by law enforcement. For example, a checkpoint could be where police set up roadblocks so they can stop and inspect all (or almost all) drivers and vehicles that pass through.',
     OtherMotorVehicleViolation:
