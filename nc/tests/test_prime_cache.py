1d4aae4737f28282552a15db81f2ea34b2911ab5
Colin Copeland <copelco@caktusgroup.com>
2024-10-29 15:50:22 -0400

    add initial cache primer tests

--- nc/tests/test_prime_cache.py
+++ nc/tests/test_prime_cache.py
@@ -9,17 +9,23 @@ def group_urls():
     prime_cache.API_ENDPOINT_NAMES = ("nc:arrests-percentage-of-stops",)
 
 
-def test_get_group_urls_empty_allowed_hosts(settings):
-    settings.ALLOWED_HOSTS = []
-    assert (
-        prime_cache.get_group_urls(agency_id=99)[0]
-        == "http://127.0.0.1:8000/api/agency/99/arrests-percentage-of-stops/"
-    )
+class TestGetGroupUrls:
+    def test_get_group_urls_empty_allowed_hosts(self, settings):
+        settings.ALLOWED_HOSTS = []
+        assert (
+            prime_cache.get_group_urls(agency_id=99)[0]
+            == "http://127.0.0.1:8000/api/agency/99/arrests-percentage-of-stops/"
+        )
 
+    def test_get_group_urls_allowed_hosts(self, settings):
+        settings.ALLOWED_HOSTS = ["nccopwatch.org"]
+        assert (
+            prime_cache.get_group_urls(agency_id=99)[0]
+            == "https://nccopwatch.org/api/agency/99/arrests-percentage-of-stops/"
+        )
 
-def test_get_group_urls_allowed_hosts(settings):
-    settings.ALLOWED_HOSTS = ["nccopwatch.org"]
-    assert (
-        prime_cache.get_group_urls(agency_id=99)[0]
-        == "https://nccopwatch.org/api/agency/99/arrests-percentage-of-stops/"
-    )
+    def test_get_group_urls_officer_id(self):
+        assert (
+            prime_cache.get_group_urls(agency_id=99, officer_id=100)[0]
+            == "https://testserver/api/agency/99/arrests-percentage-of-stops/?officer=100"
+        )
