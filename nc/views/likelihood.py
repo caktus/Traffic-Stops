657b4ea5f8392bd957b8f953bbe689cfa9bc2d86
simonkagwi <skagwi@caktusgroup.com>
2025-09-04 15:52:56 +0300

    Fix ValueError when computing likelihood of stop. (#358)
    * Fix ValueError when computing likelihood of stop.
    
    * Fix ValueError when computing likelihood of stop.

--- nc/views/likelihood.py
+++ nc/views/likelihood.py
@@ -1,4 +1,5 @@
 import django_filters
+import numpy as np
 import pandas as pd
 
 from django.db.models import Avg, Sum
@@ -102,7 +103,10 @@ def likelihood_stop_query(request, agency_id, debug=True):
     )
     if not df_acs.empty:
         # Calculate rates
-        df["stop_rate"] = df["stops"] / df["population"]
+        # If stops is non-zero and population is 0 for a race, its stop_rate will
+        # be Infinity, which will cause a ValueError when serializing to JSON.
+        # Update it to 0 in such cases
+        df["stop_rate"] = (df["stops"] / df["population"]).replace(np.inf, 0)
         df["baseline_rate"] = df[df["race"] == "White"]["stop_rate"].iloc[0]
         df["stop_rate_ratio"] = df["stop_rate"] / df["baseline_rate"]
     else:
